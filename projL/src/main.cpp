c759d40faf98d0800b99a3e3bc73a5a6d34e0ffd
SnakeCcama <97047130+SnakeCcama@users.noreply.github.com>
2025-11-18 20:12:27 +0300

    Update main.cpp

--- projL/src/main.cpp
+++ projL/src/main.cpp
@@ -78,6 +78,51 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposln)
    camera.ProcessMouseMovement(xoffset, yoffset);   
 }
 
+unsigned int loadTexture(const std::string& texturefilePath)
+{
+
+    unsigned int texture;
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+
+
+    // ---------
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    // load image, create texture and generate mipmaps
+    unsigned char* data = stbi_load(std::filesystem::path(texturefilePath).c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+
+        GLenum format;
+        if(nrChannels == 1)
+            format = GL_RED;
+        else if (nrChannels == 3)
+            format = GL_RGB;
+        else if (nrChannels == 4)
+            format = GL_RGBA;
+        
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+
+    stbi_image_free(data);
+
+    return texture;
+
+   
+}
 
 
 int main()
